0c3b1c83c66f89e8ea904cb83dd8c16b714012f4
Mrinal Anand <mrinal.anand9123@gmail.com>
2023-11-24 07:43:03 +0530

    d

--- src/components/HitsComponent.tsx
+++ src/components/HitsComponent.tsx
@@ -1,12 +1,25 @@
 import Link from 'next/link';
-const HitsComponent = ({ hit}) => {
+import React from 'react';
 
-    
-    return (
-            <div className="algo">
-                <p>{hit.title}</p>
-            </div>
-    );
-  };
-  
-  export default HitsComponent;
+interface ContentfulDataItem {
+  objectID: string;
+  title: string;
+  content: string;
+  slug: string;
+  excerpt: string;
+}
+
+const HitsComponent: React.FC<{ hit: ContentfulDataItem }> = ({ hit }) => {
+  return (
+    <div>
+      <h2>
+        <Link href={`/posts/${hit.slug}`}>
+          <a>{hit.title}</a>
+        </Link>
+      </h2>
+      <p>{hit.excerpt}</p>
+    </div>
+  );
+};
+
+export default HitsComponent;
